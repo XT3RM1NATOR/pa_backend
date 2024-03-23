f8b376e090ef91d31c77c6f2c0156dbce1ab1282
MacOS <macos@MacBook-Air-MacOS.local>
2024-03-23 13:42:03 +0500

    update

--- cmd/pointai/main.go
+++ cmd/pointai/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/Point-AI/backend/config"
-	app "github.com/Point-AI/backend/internal/app/db"
+	"github.com/Point-AI/backend/internal/app/db"
 	"github.com/Point-AI/backend/internal/app/server"
 )
 
 func main() {
 	cfg := config.Load()
-	db := app.ConnectToDB(cfg)
+	mongodb := db.ConnectToDB(cfg)
 
-	server.RunHTTPServer(cfg, db)
+	server.RunHTTPServer(cfg, mongodb)
 }
